d2af64339a8fcff073d45ab1dd822eea1ac0c9b7
pankaj-tandure <pankajtandure@gmail.com>
2025-12-22 15:46:42 +0530

    Initial commit
    Initial commit

---
+++ src/main/java/com/java/gcp_demo/controller/ProductController.java
@@ -0,0 +1,73 @@
+package com.java.gcp_demo.controller;
+
+import com.java.gcp_demo.model.Product;
+import com.java.gcp_demo.repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@Transactional
+public class ProductController {
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/all")
+    public List<Product> getAllProducts() {
+
+        Product product = new Product();
+        product.setName("Test");
+        product.setPrice(BigDecimal.valueOf(10.0));
+        product.setId(1L);
+        product.setDescription("test test");
+
+        List<Product> list = new ArrayList<>();
+        list.add(product);
+
+        return productRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    existingProduct.setName(productDetails.getName());
+                    existingProduct.setDescription(productDetails.getDescription());
+                    existingProduct.setPrice(productDetails.getPrice());
+                    return ResponseEntity.ok(productRepository.save(existingProduct));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .map(product -> {
+                    productRepository.delete(product);
+                    return ResponseEntity.ok().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
+
---
+++ src/main/java/com/java/gcp_demo/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.java.gcp_demo.repository;
+
+import com.java.gcp_demo.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByNameContaining(String name);
+}
